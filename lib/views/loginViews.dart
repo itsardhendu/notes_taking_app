9b9901ba4c15f4484ad9f0329bb9ba6fe040f16f
Ardhendu Sarkar <58818057+itsardhendu@users.noreply.github.com>
2023-08-04 21:31:32 +0530

    login View and Register View created

---
+++ lib/views/loginViews.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: TextButton(
+        onPressed:  () {
+
+        }, child: const Text('Login'),
+      ),
+    );
+  }
+}
---
+++ lib/views/registerViews.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+          ),
+          TextField(
+            controller: _password,
+          ),
+          TextButton(
+            onPressed: () async {},
+            child: const Text('Click to Register'),
+          ),
+        ],
+      ),
+    );
+  }
+}
