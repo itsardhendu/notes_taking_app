00179b24bbc44967d1e0178196cb69af965df3bc
Ardhendu Sarkar <58818057+itsardhendu@users.noreply.github.com>
2023-08-21 00:59:49 +0530

    Routes linked with loginViews

--- lib/views/registerViews.dart
+++ lib/views/registerViews.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -59,18 +60,18 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCreadential = await FirebaseAuth.instance
+                final userCredential = await FirebaseAuth.instance
                     .createUserWithEmailAndPassword(
                         email: email,
                         password: password); //Firebase Create User
-                print(userCreadential);
+                devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
-                  print('Weak password');
+                  devtools.log('Weak password');
                 } else if (e.code == 'email-already-in-use') {
-                  print('Email is already in use');
+                  devtools.log('Email is already in use');
                 } else if (e.code == 'invalid-email') {
-                  print('Invalid email entered');
+                  devtools.log('Invalid email entered');
                 }
               }
             },
